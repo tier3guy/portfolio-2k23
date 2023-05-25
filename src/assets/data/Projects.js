ea511eb6da5e22d4b14899268004f24e966093e4
Avinash Gupta <avinash.gupta@vils.ai>
2023-05-25 10:14:20 +0530

    project added

--- src/assets/data/Projects.js
+++ src/assets/data/Projects.js
@@ -17,6 +17,15 @@ const ProjectsData = [
     live: "https://drive.google.com/file/d/12gYy1uwkJdgSPHZb5n16RbS7tNOgXnag/view?usp=sharing",
     tech: ["React Native", "Node JS / Express JS", "Mongo DB", "Deep AI API"],
   },
+  {
+    id: 3,
+    title: "CrowdFunding - Web App",
+    description:
+      "Led the development of a robust full-stack web application using React, TypeScript, and Tailwind CSS. Built a user-friendly interface that allows individuals to create and participate in crowdfunding campaigns, empowering them to showcase their projects and receive financial support. Leveraged a third-party web service for seamless campaign management, including real-time updates on funding progress and milestones. Implemented secure user authentication and account management functionalities. Collaborated closely with a talented team, utilizing agile methodologies and Git for efficient project coordination and version control.",
+    gh: "https://github.com/tier3guy/crowdfunding.git",
+    live: "https://crowdfunding-peach.vercel.app/",
+    tech: ["React", "Typescript", "Tailwind", "Thirdweb", "Solidity"],
+  },
 ];
 
 export default ProjectsData;
