c56eed6756f9030c2bee6e8d00b04624029922d3
Avinash Gupta <avinash.gupta@vils.ai>
2023-05-31 10:38:57 +0530

    Error Page Added

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,27 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
   return (
     <div className="error-container center">
-      <h1>Hi, welcome to avinash templates / react</h1>
+      <h1>404</h1>
+      <p>Maybe Bigfoot has broken this page. Comeback to the homepage.</p>
+      <Link to="/">
+        <p className="border-bottom" style={{ marginTop: 20 }}>
+          Go to the Homepage
+        </p>
+      </Link>
+      {/* <span
+        style={{
+          color: "var(--orange)",
+          position: "absolute",
+          bottom: 10,
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          fontSize: "3em",
+        }}
+      >
+        avinash
+      </span> */}
     </div>
   );
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,15 @@ import { Home, ErrorPage } from "../pages";
 const routes = [
   {
     key: "1",
+    exact: "true",
     path: "/",
     element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    key: "404",
+    path: "*",
+    element: <ErrorPage />,
     errorElement: <ErrorPage />,
   },
 ];
